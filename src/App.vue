76142bdef0af552f4aeb6cf758e60c7b9e2c02af
Merian Lesster <merianlesster27.ml@gmail.com>
2025-11-13 19:25:01 +0100

    feat: add PrimeVue and PrimeIcons packages

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <h1>Hiii. there</h1>
+  <Button label="Profile" icon="pi pi-user" />
+
   <RouterView />
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import Button from 'primevue/button'
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import './assets/main.css'
+import 'primeicons/primeicons.css'
 
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
@@ -8,6 +11,14 @@ import { router } from './router'
 
 const app = createApp(App)
 
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: 'none',
+    },
+  },
+})
 app.use(createPinia())
 app.use(router)
 
